09241169f426896c7ff176bb02a3ac564008e57d
tonybarroslimon <tonybarroslimon@outlook.com>
2014-04-23 02:12:47 -0700

    added some comments, still working on adding comments. Also, fixed some new bugs and optimized the code a bit

--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,45 @@
+## The function enterData creates the inital data frame used to merge the data together
+## when it is passed the name of the file and the path where that file can be located. 
+## This function also subsets the data to pull out only the mean and standard deviation
+## measurements.
+
 enterData <- function(file_name, path_name) {
+        
+        # creates the path to the file with the 'y' name and loads that information as a data table.
         file_path <- file.path(path_name, paste("y_", file_name, ".txt", sep = ""))
         yFile <- read.table(file_path, header = FALSE, col.names = c("ActivityID"))
         
+        # creates the path to the subject file and loads that information as a data table.
         file_path <- file.path(path_name, paste("subject_", file_name, ".txt", sep = ""))
         subject <- read.table(file_path, header = FALSE, col.names = c("SubjectID"))
         
+        # reads the column names and loads them as a data table.
         dataColumns <- read.table("features.txt", header = FALSE, as.is = TRUE, col.names = c("MeasureID", "MeasureName"))
         
+        # creates the path to the file with the 'x' name and loads that information as a data table.
         file_path <- file.path(path_name, paste("X_", file_name, ".txt", sep = ""))
         xFile <- read.table(file_path, header = FALSE, col.names = dataColumns$MeasureName)
         
+        # creates the names of the columns required for the data file.
         sub_data_columns <- grep(".*mean\\(\\) |.*std\\(\\)", dataColumns$MeasureName)
         
+        # creates the X data file subsetting the data column names done in the previous step.
         data <- xFile[, sub_data_columns]
         
+        # appends the subject and activity ID columns.
         data$ActivityID <- yFile$ActivityID
         data$SubjectID <- subject$SubjectID
         
+        # returns the data table.
         data
 }
 
+# the testData function reads the test data in the test folder and uses the file with the test name.
 testData <- function() {
         enterData("test", "test")
 }
 
+# the trainData function reads the train data in the train folder and uses the file with the train name.
 trainData <- function() {
         enterData("train", "train")
 }
@@ -37,7 +53,7 @@ mergeData <- function() {
         data
 }
 
-applyActivityLabel <- function(data) {
+addActivityLabel <- function(data) {
         activity_labels <- read.table("activity_labels.txt", header=F, as.is=T, col.names=c("ActivityID", "ActivityName"))
         activity_labels$ActivityName <- as.factor(activity_labels$ActivityName)
         data_labeled <- merge(data, activity_labels)
@@ -45,10 +61,10 @@ applyActivityLabel <- function(data) {
 }
 
 getMergedLabeledData <- function() {
-        applyActivityLabel(mergeData())
+        addActivityLabel(mergeData())
 }
 
-getTidyData <- function(merged_labeled_data) {
+getData <- function(merged_labeled_data) {
         library(reshape2)
         
         id_vars = c("ActivityID", "ActivityName", "SubjectID")
@@ -58,9 +74,9 @@ getTidyData <- function(merged_labeled_data) {
         dcast(melted_data, ActivityName + SubjectID ~ variable, mean)    
 }
 
-createTidyDataFile <- function(fname) {
-        tidy_data <- getTidyData(getMergedLabeledData())
+createDataFile <- function(fname) {
+        tidy_data <- getData(getMergedLabeledData())
         write.table(tidy_data, fname)
 }
 
-createTidyDataFile("tidy.txt")
+createDataFile("wearable_computing_analysis.txt")
